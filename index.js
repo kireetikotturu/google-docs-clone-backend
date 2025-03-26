6072b6c17a7a23485b836208bf39ad0599d812be
Chandra Kireeti Kotturu <104622340+kireetikotturu@users.noreply.github.com>
2025-03-26 18:15:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -45,8 +45,9 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-  .then(() => console.log("✅ Connected to MongoDB"))
-  .catch(err => console.error("❌ MongoDB connection error:", err));
+.then(() => console.log("✅ Connected to MongoDB Atlas"))
+.catch((err) => console.error("❌ MongoDB connection error:", err));
+
 
 
 // ✅ Save Letter to MongoDB & Google Drive
